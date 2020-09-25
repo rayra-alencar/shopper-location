fa5d756f0d6719c5b648bb6579176238d2a3b4cf
polishq <arthur.bond@vtex.com.br>
2020-09-25 13:44:57 -0400

    Adjust priority of Google response fields used to determine shopper's city, update eslint version, linting

--- react/helpers/getParsedAddress.tsx
+++ react/helpers/getParsedAddress.tsx
@@ -1,6 +1,9 @@
-import { path } from 'ramda'
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 const getCountryISO3 = require('country-iso-2-to-3')
+
 /**
  * The place object returned from Google Maps API has some extra informations about the address that won't be used when sending
  * to orderform. So, this function will reduce nested address information into a simpler consumable object.
@@ -8,22 +11,26 @@ const getCountryISO3 = require('country-iso-2-to-3')
  * @param {Object} place The place object returned from Google Maps API
  * @returns {Object} The reduced address data with only necessary fields/information
  */
-export const getParsedAddress = (place: { address_components: any[] }) => {
+export const getParsedAddress = (place: {
+  address_components: any[]
+  geometry: any
+}) => {
   const parsedAddressComponents = place.address_components.reduce(
     (accumulator, address) => {
       const parsedItem = address.types.reduce(
-        (accumulator: any, type: any) => ({
-          ...accumulator,
+        (typeAccumulator: any, type: any) => ({
+          ...typeAccumulator,
           [type]: address.short_name,
         }),
         {}
       )
+
       return { ...accumulator, ...parsedItem }
     },
     {}
   )
 
-  const { lat, lng } = path(['geometry', 'location'], place) || {}
+  const { lat, lng } = place.geometry?.location ?? {}
   // lat and lng may come as a function or a double
   const latitude = typeof lat === 'function' ? lat() : lat
   const longitude = typeof lng === 'function' ? lng() : lng
@@ -32,8 +39,8 @@ export const getParsedAddress = (place: { address_components: any[] }) => {
     addressType: 'residential',
     city:
       parsedAddressComponents.administrative_area_level_3 ||
-      parsedAddressComponents.administrative_area_level_2 ||
-      parsedAddressComponents.locality,
+      parsedAddressComponents.locality ||
+      parsedAddressComponents.administrative_area_level_2,
     // complement: '',
     /* Google Maps API returns Alpha-2 ISO codes, but checkout API requires Alpha-3 */
     country: getCountryISO3(parsedAddressComponents.country),
@@ -44,7 +51,7 @@ export const getParsedAddress = (place: { address_components: any[] }) => {
     state: parsedAddressComponents.administrative_area_level_1,
     street: `${
       parsedAddressComponents.street_number
-        ? parsedAddressComponents.street_number + ' '
+        ? `${parsedAddressComponents.street_number} `
         : ''
     }${parsedAddressComponents.route}`,
     geoCoordinates: latitude && longitude ? [longitude, latitude] : null,
